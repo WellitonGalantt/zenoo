3e2aedcc34dfb925e8502886ff08f11ac52b8aae
WellitonGalantt <devcode.welliton@gmail.com>
2025-06-07 21:55:44 -0300

    Implement the encrypt password using Bcrypt

---
+++ src/infra/providers/bcrypt/hash.provider.ts
@@ -0,0 +1,16 @@
+import { HashProvider } from "../../../application/providers/hash.provider";
+import bcrypt from "bcrypt";
+
+export class IHashProvider implements HashProvider {
+    private readonly saltRounds: number = 8;
+
+    public async encrypt(rawPassword: string): Promise<string> {
+        const hashedPassword = await bcrypt.hash(rawPassword, this.saltRounds);
+
+        return hashedPassword;
+    }
+
+    public async verifyHash(hashedPassword: string, rawPassword: string): Promise<boolean> {
+        return await bcrypt.compare(rawPassword, hashedPassword);
+    }
+}
